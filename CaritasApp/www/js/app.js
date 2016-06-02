362c0c472b702fdafd742c951a8f998a855ca92f
IvoVidovic <a1406309@unet.univie.ac.at>
2016-06-02 14:36:13 +0200

    Abgabeversion M3

--- CaritasApp/www/js/app.js
+++ CaritasApp/www/js/app.js
@@ -33,28 +33,136 @@ angular.module('app', ['ionic', 'app.controllers', 'app.services'])
     
   
 
-      .state('startseite', {
+      .state('start', {
     url: '/starseite',
-    templateUrl: 'templates/startseite.html',
-    controller: 'startseiteCtrl'
+    templateUrl: 'templates/start.html',
+    controller: 'startCtrl'
   })
 
-  .state('spender', {
+  .state('spenden', {
     url: '/spenderSeite',
-    templateUrl: 'templates/spender.html',
-    controller: 'spenderCtrl'
+    templateUrl: 'templates/spenden.html',
+    controller: 'spendenCtrl'
   })
 
-  .state('hilfe', {
+  .state('asylzentrum', {
+    url: '/Asylzentrum',
+    templateUrl: 'templates/asylzentrum.html',
+    controller: 'asylzentrumCtrl'
+  })
+
+  .state('asylrechtsberatung', {
+    url: '/Asylrechtsberatung',
+    templateUrl: 'templates/asylrechtsberatung.html',
+    controller: 'asylrechtsberatungCtrl'
+  })
+
+  .state('treffpunktSterreich', {
+    url: '/Treffpunkt Österreich',
+    templateUrl: 'templates/treffpunktSterreich.html',
+    controller: 'treffpunktSterreichCtrl'
+  })
+
+  .state('begegnungSterreich', {
+    url: '/Begegnung Österreich',
+    templateUrl: 'templates/begegnungSterreich.html',
+    controller: 'begegnungSterreichCtrl'
+  })
+
+  .state('deutschkurse', {
+    url: '/Deutschkurse',
+    templateUrl: 'templates/deutschkurse.html',
+    controller: 'deutschkurseCtrl'
+  })
+
+  .state('carbizBildungsberatung', {
+    url: '/Carbiz - Bildungsberatung',
+    templateUrl: 'templates/carbizBildungsberatung.html',
+    controller: 'carbizBildungsberatungCtrl'
+  })
+
+  .state('baleh', {
+    url: '/Baleh',
+    templateUrl: 'templates/baleh.html',
+    controller: 'balehCtrl'
+  })
+
+  .state('lerncafS', {
+    url: '/Lerncafés',
+    templateUrl: 'templates/lerncafS.html',
+    controller: 'lerncafSCtrl'
+  })
+
+  .state('psychosozialeAngebote', {
+    url: '/page37',
+    templateUrl: 'templates/psychosozialeAngebote.html',
+    controller: 'psychosozialeAngeboteCtrl'
+  })
+
+  .state('hausAmadou', {
+    url: '/Haus Amadou',
+    templateUrl: 'templates/hausAmadou.html',
+    controller: 'hausAmadouCtrl'
+  })
+
+  .state('notquartiereFRFlChtlinge', {
+    url: '/Notquartiere für Flüchtlinge',
+    templateUrl: 'templates/notquartiereFRFlChtlinge.html',
+    controller: 'notquartiereFRFlChtlingeCtrl'
+  })
+
+  .state('wGsFRUnbegleiteteMinderjHrigeFlChtlinge', {
+    url: '/WGs für unbegleitete minderjährige Flüchtlinge',
+    templateUrl: 'templates/wGsFRUnbegleiteteMinderjHrigeFlChtlinge.html',
+    controller: 'wGsFRUnbegleiteteMinderjHrigeFlChtlingeCtrl'
+  })
+
+  .state('wohnhUsereFlChtlinge', {
+    url: '/Wohnhäuser',
+    templateUrl: 'templates/wohnhUsereFlChtlinge.html',
+    controller: 'wohnhUsereFlChtlingeCtrl'
+  })
+
+  .state('initiativeMedicalAidForRefugees', {
+    url: '/Initiative Medical Aid for Refugees',
+    templateUrl: 'templates/initiativeMedicalAidForRefugees.html',
+    controller: 'initiativeMedicalAidForRefugeesCtrl'
+  })
+
+  .state('hilfeSuchen', {
     url: '/hilfe',
-    templateUrl: 'templates/hilfe.html',
-    controller: 'hilfeCtrl'
+    templateUrl: 'templates/hilfeSuchen.html',
+    controller: 'hilfeSuchenCtrl'
   })
 
-  .state('altePerson', {
-    url: '/altePerson',
-    templateUrl: 'templates/altePerson.html',
-    controller: 'altePersonCtrl'
+  .state('asylwerber', {
+    url: '/auflistung',
+    templateUrl: 'templates/asylwerber.html',
+    controller: 'asylwerberCtrl'
+  })
+
+  .state('beratungFRAsylwerberInnen', {
+    url: '/Beratung für AsylwerberInnen',
+    templateUrl: 'templates/beratungFRAsylwerberInnen.html',
+    controller: 'beratungFRAsylwerberInnenCtrl'
+  })
+
+  .state('wohnen', {
+    url: '/wohnen',
+    templateUrl: 'templates/wohnen.html',
+    controller: 'wohnenCtrl'
+  })
+
+  .state('ausbildung', {
+    url: '/ausbildung',
+    templateUrl: 'templates/ausbildung.html',
+    controller: 'ausbildungCtrl'
+  })
+
+  .state('kontakt', {
+    url: '/kontaktAltePerson',
+    templateUrl: 'templates/kontakt.html',
+    controller: 'kontaktCtrl'
   })
 
   .state('alleinerziehendePerson', {
@@ -81,16 +189,184 @@ angular.module('app', ['ionic', 'app.controllers', 'app.services'])
     controller: 'rechtlichesCtrl'
   })
 
-  .state('kontakt', {
+  .state('asylIntegration', {
+    url: '/page11',
+    templateUrl: 'templates/asylIntegration.html',
+    controller: 'asylIntegrationCtrl'
+  })
+
+  .state('kontakt2', {
     url: '/allein_5_Kontakt',
-    templateUrl: 'templates/kontakt.html',
-    controller: 'kontaktCtrl'
+    templateUrl: 'templates/kontakt2.html',
+    controller: 'kontakt2Ctrl'
   })
 
   .state('literaturempfehlungen', {
     url: '/allein_4_literatur',
     templateUrl: 'templates/literaturempfehlungen.html',
     controller: 'literaturempfehlungenCtrl'
+  })
+
+  .state('pflege', {
+    url: '/altePerson1',
+    templateUrl: 'templates/pflege.html',
+    controller: 'pflegeCtrl'
+  })
+
+  .state('pflegeZuhause', {
+    url: '/altePersonPflegeZuhause',
+    templateUrl: 'templates/pflegeZuhause.html',
+    controller: 'pflegeZuhauseCtrl'
+  })
+
+  .state('24StundenBetreuung', {
+    url: '/altePerson24H',
+    templateUrl: 'templates/24StundenBetreuung.html',
+    controller: '24StundenBetreuungCtrl'
+  })
+
+  .state('mobilesHospiz', {
+    url: '/altePersonMobilesHospiz',
+    templateUrl: 'templates/mobilesHospiz.html',
+    controller: 'mobilesHospizCtrl'
+  })
+
+  .state('pflegewohnhUser', {
+    url: '/altePersonPflegewohnhaeuser',
+    templateUrl: 'templates/pflegewohnhUser.html',
+    controller: 'pflegewohnhUserCtrl'
+  })
+
+  .state('unsereHUser', {
+    url: '/altePersonHaeuser',
+    templateUrl: 'templates/unsereHUser.html',
+    controller: 'unsereHUserCtrl'
+  })
+
+  .state('notruftelefon', {
+    url: '/altePersonNotruftelefon',
+    templateUrl: 'templates/notruftelefon.html',
+    controller: 'notruftelefonCtrl'
+  })
+
+  .state('obdachWohnen', {
+    url: '/obdach_wohnen',
+    templateUrl: 'templates/obdachWohnen.html',
+    controller: 'obdachWohnenCtrl'
+  })
+
+  .state('beratung', {
+    url: '/beratung',
+    templateUrl: 'templates/beratung.html',
+    controller: 'beratungCtrl'
+  })
+
+  .state('mobileNotversorgung', {
+    url: '/mobile_notversorgung',
+    templateUrl: 'templates/mobileNotversorgung.html',
+    controller: 'mobileNotversorgungCtrl'
+  })
+
+  .state('notschlafstellen', {
+    url: '/notschlafstellen',
+    templateUrl: 'templates/notschlafstellen.html',
+    controller: 'notschlafstellenCtrl'
+  })
+
+  .state('tagesaufenthalt', {
+    url: '/tagesaufenthalt',
+    templateUrl: 'templates/tagesaufenthalt.html',
+    controller: 'tagesaufenthaltCtrl'
+  })
+
+  .state('mobileWohnbetreuung', {
+    url: '/mobile_wohnbetreuung',
+    templateUrl: 'templates/mobileWohnbetreuung.html',
+    controller: 'mobileWohnbetreuungCtrl'
+  })
+
+  .state('sozialRCkkehrberatung', {
+    url: '/sozial_und_rueckkehrberatung',
+    templateUrl: 'templates/sozialRCkkehrberatung.html',
+    controller: 'sozialRCkkehrberatungCtrl'
+  })
+
+  .state('p7', {
+    url: '/p7_wiener_service_fuer_wohnungslose',
+    templateUrl: 'templates/p7.html',
+    controller: 'p7Ctrl'
+  })
+
+  .state('wohnungssicherungNOst', {
+    url: '/wohnungssicherung_noe_ost',
+    templateUrl: 'templates/wohnungssicherungNOst.html',
+    controller: 'wohnungssicherungNOstCtrl'
+  })
+
+  .state('tageszentrumAmHauptbahnhof', {
+    url: '/tageszentrum_am_hauptbahnhof',
+    templateUrl: 'templates/tageszentrumAmHauptbahnhof.html',
+    controller: 'tageszentrumAmHauptbahnhofCtrl'
+  })
+
+  .state('betreuunggszentrumGruft', {
+    url: '/betreuungszentrum_gruft',
+    templateUrl: 'templates/betreuunggszentrumGruft.html',
+    controller: 'betreuunggszentrumGruftCtrl'
+  })
+
+  .state('zweiteGruft', {
+    url: '/zweite_gruft',
+    templateUrl: 'templates/zweiteGruft.html',
+    controller: 'zweiteGruftCtrl'
+  })
+
+  .state('wRmestuben', {
+    url: '/waermestuben',
+    templateUrl: 'templates/wRmestuben.html',
+    controller: 'wRmestubenCtrl'
+  })
+
+  .state('streetwork', {
+    url: '/streetwork',
+    templateUrl: 'templates/streetwork.html',
+    controller: 'streetworkCtrl'
+  })
+
+  .state('canisibus', {
+    url: '/canisibus',
+    templateUrl: 'templates/canisibus.html',
+    controller: 'canisibusCtrl'
+  })
+
+  .state('kLtetelefon', {
+    url: '/kaeltetelefon',
+    templateUrl: 'templates/kLtetelefon.html',
+    controller: 'kLtetelefonCtrl'
+  })
+
+  .state('notschlafstellenFRFrauen', {
+    url: '/notschlafstellen_frauen',
+    templateUrl: 'templates/notschlafstellenFRFrauen.html',
+    controller: 'notschlafstellenFRFrauenCtrl'
+  })
+
+  .state('notschlafstellenFRMNner', {
+    url: '/notschlafstellen_maenner',
+    templateUrl: 'templates/notschlafstellenFRMNner.html',
+    controller: 'notschlafstellenFRMNnerCtrl'
+  })
+
+  .state('notschlafstellenFRJugendliche', {
+    url: '/notschlafstellen_jugendliche',
+    templateUrl: 'templates/notschlafstellenFRJugendliche.html',
+    controller: 'notschlafstellenFRJugendlicheCtrl'
+  })
+
+  .state('louiseBus', {
+    url: '/louise_bus',
+    templateUrl: 'templates/louiseBus.html',
+    controller: 'louiseBusCtrl'
   })
 
 $urlRouterProvider.otherwise('/starseite')
--- CaritasApp/www/js/controllers.js
+++ CaritasApp/www/js/controllers.js
@@ -1,18 +1,90 @@
 angular.module('app.controllers', [])
   
-.controller('startseiteCtrl', function($scope) {
+.controller('startCtrl', function($scope) {
 
 })
    
-.controller('spenderCtrl', function($scope) {
+.controller('spendenCtrl', function($scope) {
 
 })
    
-.controller('hilfeCtrl', function($scope) {
+.controller('asylzentrumCtrl', function($scope) {
 
 })
    
-.controller('altePersonCtrl', function($scope) {
+.controller('asylrechtsberatungCtrl', function($scope) {
+
+})
+   
+.controller('treffpunktSterreichCtrl', function($scope) {
+
+})
+   
+.controller('begegnungSterreichCtrl', function($scope) {
+
+})
+   
+.controller('deutschkurseCtrl', function($scope) {
+
+})
+   
+.controller('carbizBildungsberatungCtrl', function($scope) {
+
+})
+   
+.controller('balehCtrl', function($scope) {
+
+})
+   
+.controller('lerncafSCtrl', function($scope) {
+
+})
+   
+.controller('psychosozialeAngeboteCtrl', function($scope) {
+
+})
+   
+.controller('hausAmadouCtrl', function($scope) {
+
+})
+   
+.controller('notquartiereFRFlChtlingeCtrl', function($scope) {
+
+})
+   
+.controller('wGsFRUnbegleiteteMinderjHrigeFlChtlingeCtrl', function($scope) {
+
+})
+   
+.controller('wohnhUsereFlChtlingeCtrl', function($scope) {
+
+})
+   
+.controller('initiativeMedicalAidForRefugeesCtrl', function($scope) {
+
+})
+   
+.controller('hilfeSuchenCtrl', function($scope) {
+
+})
+   
+.controller('asylwerberCtrl', function($scope) {
+
+})
+   
+.controller('beratungFRAsylwerberInnenCtrl', function($scope) {
+
+})
+   
+.controller('wohnenCtrl', function($scope) {
+
+})
+   
+.controller('ausbildungCtrl', function($scope) {
+
+})
+   
+.controller('kontaktCtrl', function($scope) {
 
 })
    
@@ -32,11 +104,123 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('kontaktCtrl', function($scope) {
+.controller('asylIntegrationCtrl', function($scope) {
+
+})
+   
+.controller('kontakt2Ctrl', function($scope) {
 
 })
    
 .controller('literaturempfehlungenCtrl', function($scope) {
+
+})
+   
+.controller('pflegeCtrl', function($scope) {
+
+})
+   
+.controller('pflegeZuhauseCtrl', function($scope) {
+
+})
+   
+.controller('24StundenBetreuungCtrl', function($scope) {
+
+})
+   
+.controller('mobilesHospizCtrl', function($scope) {
+
+})
+   
+.controller('pflegewohnhUserCtrl', function($scope) {
+
+})
+   
+.controller('unsereHUserCtrl', function($scope) {
+
+})
+   
+.controller('notruftelefonCtrl', function($scope) {
+
+})
+   
+.controller('obdachWohnenCtrl', function($scope) {
+
+})
+   
+.controller('beratungCtrl', function($scope) {
+
+})
+   
+.controller('mobileNotversorgungCtrl', function($scope) {
+
+})
+   
+.controller('notschlafstellenCtrl', function($scope) {
+
+})
+   
+.controller('tagesaufenthaltCtrl', function($scope) {
+
+})
+   
+.controller('mobileWohnbetreuungCtrl', function($scope) {
+
+})
+   
+.controller('sozialRCkkehrberatungCtrl', function($scope) {
+
+})
+   
+.controller('p7Ctrl', function($scope) {
+
+})
+   
+.controller('wohnungssicherungNOstCtrl', function($scope) {
+
+})
+   
+.controller('tageszentrumAmHauptbahnhofCtrl', function($scope) {
+
+})
+   
+.controller('betreuunggszentrumGruftCtrl', function($scope) {
+
+})
+   
+.controller('zweiteGruftCtrl', function($scope) {
+
+})
+   
+.controller('wRmestubenCtrl', function($scope) {
+
+})
+   
+.controller('streetworkCtrl', function($scope) {
+
+})
+   
+.controller('canisibusCtrl', function($scope) {
+
+})
+   
+.controller('kLtetelefonCtrl', function($scope) {
+
+})
+   
+.controller('notschlafstellenFRFrauenCtrl', function($scope) {
+
+})
+   
+.controller('notschlafstellenFRMNnerCtrl', function($scope) {
+
+})
+   
+.controller('notschlafstellenFRJugendlicheCtrl', function($scope) {
+
+})
+   
+.controller('louiseBusCtrl', function($scope) {
 
 })
  
